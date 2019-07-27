52e1f30e9cbd3ec446890354af7dcb33f8c764e0
exgamext <53259051+exgamext@users.noreply.github.com>
2019-07-27 04:43:28 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1564213325,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"background-sheet0.png",
+		"moneynote-sheet0.png",
+		"moneybackground-sheet0.png",
+		"lojabutton-sheet0.png",
+		"configbutton-sheet0.png",
+		"logo-sheet0.png",
+		"loja-sheet0.png",
+		"back-sheet0.png",
+		"click1-sheet0.png",
+		"click2-sheet0.png",
+		"carregar-sheet0.png",
+		"salvar-sheet0.png",
+		"resetar-sheet0.png",
+		"configurações-sheet0.png",
+		"style.css"
+	]
+}
